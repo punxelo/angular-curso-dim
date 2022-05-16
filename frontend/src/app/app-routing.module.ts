25533256007f79f1d0761f6375a967d60405065b
DIglesias <danygle86@gmail.com>
2022-05-17 01:28:57 +0200

    Completada estrucuta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+    },
+    {
+    path: "not-found",
+    component: NotFoundComponent,
+    },
+    {
+    path: "**",
+    redirectTo: "not-found",
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ShellComponent } from './shell/shell.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
 import { FooterComponent } from './shell/footer/footer.component';
-import { NotFoundComponent } from './shell/not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -20,7 +25,8 @@ import { NotFoundComponent } from './shell/not-found/not-found.component';
     CommonModule,
     AppRoutingModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   exports: [ShellComponent]
 })
